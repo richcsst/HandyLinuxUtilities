4944d8c8eb780396ab6b9a25c1db928574aca47d
Richard Kelsch <rich@csst.net>
2026-07-05 07:37:26 -0600

    Rewrote for new API

--- wimip.pl
+++ wimip.pl
@@ -2,27 +2,30 @@
 
 use strict;
 use LWP::Simple;
-
+use JSON::Parse 'parse_json';
 # wimip.pl [key]
 if (scalar(@ARGV)) {
     if (exists($ENV{'WHATISMYIP_INFO'}) && defined($ENV{'WHATISMYIP_INFO'}) && $ENV{'WHATISMYIP_INFO'} ne '') {
         print $ENV{'WHATISMYIP_INFO'};
     } else {
-        my $wimip_url = 'https://api.whatismyip.com/ip.php?key=' . $ARGV[0];
-        my $ip;
-        unless (defined($ip = get($wimip_url))) {
+	my $key = $ARGV[0];
+        my $wimip_url = 'https://wimi-api.whatismyip.com/ip?key=' . $key;
+        my $json_ip;
+        unless (defined($json_ip = get($wimip_url))) {
             die "$wimip_url not responding as expected\n";
         }
-        chomp($ip);
-        my $wimip_info_url = 'https://api.whatismyip.com/ip-address-lookup.php?key=' . $ARGV[0] . '&input=' . $ip;
+        chomp($json_ip);
+	my $ip = parse_json($json_ip);
+        my $wimip_info_url = 'https://wimi-api.whatismyip.com/ip-address-lookup/' . $ip->{'ip'} . "?key=$key";
         my $meta;
         unless(defined($meta = get($wimip_info_url))) {
             die "$wimip_info_url not responding as expected\n";
         }
-        my @info = split(/\n/, $meta);
-        my $processed = join(';', @info);
-        $processed =~ s/\s*\;\s*/;/gs;
-        print $processed;
+	my $ip_info = parse_json($meta);
+	foreach my $name (keys %{$ip_info}) {
+	    print "export WHATISMYIP_" . uc($name) . '="' . $ip_info->{$name} . '"' . "\n";
+#	    system("export WHATISMYIP_" . uc($name) . '=' . $ip_info->{$name});
+	}
     }
 } else {
     die "Missing key\n";
